54983f89f27056811ce2c0bde594cbce0e8aec0b
unknown <604896160@qq.com>
2024-04-11 02:15:25 +0800

    0411

---
+++ Source/XV3DGS/XV3DGS.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "XV3DGS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, XV3DGS, "XV3DGS" );
